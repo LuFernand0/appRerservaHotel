ffcf6c7263cc20f0db4bf02dee0d34658f2e17a6
LuFernand0 <luizfernandocoelho13@gmail.com>
2024-02-16 22:40:22 -0300

    tabela funcionario e cliente criada

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('content')
+
+<img src="assets/fotoPrincipal.jpg" style="width:100%" />
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\FuncionarioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [ClienteController::class, 'showHome']);
+
+Route::get("/cadastro-cliente", [ClienteController::class, 'showFormularioCadastro']);
+
+Route::get("/cadastro-funcionario", [FuncionarioController::class, "showFormularioCadastro"]);
